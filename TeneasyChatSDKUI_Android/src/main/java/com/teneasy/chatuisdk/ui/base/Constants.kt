3aa3f58bab0554769a603dc2bea9e3d5dda1c6ac
xiao fu <7938813+xiaofu2024@users.noreply.github.com>
2025-07-04 22:41:19 +0800

    good

--- TeneasyChatSDKUI_Android/src/main/java/com/teneasy/chatuisdk/ui/base/Constants.kt
+++ TeneasyChatSDKUI_Android/src/main/java/com/teneasy/chatuisdk/ui/base/Constants.kt
@@ -46,13 +46,15 @@ class Constants {
         //var baseUrlImage = "https://images-3-test.qlbig05.xyz"  // 图片服务器地址
 
         // 基础配置属性
-        //var lines = "https://csapi.hfxg.xyz,https://xxxapi.qixin14.xyz"  // 服务器线路地址
         //var cert = "CMUCEAUYASCUAyjs8YWbyTI.xnc1L2bV5n21oQ2RNN8ojJD4IS-hTvXo2cgvFka4SLjsQ6rdhFFyfWKnNX3iU2VYtZtGn2-BfpI5J_xuYiy8CA"  // 认证证书
         //var cert = "COgBEAUYASDzASitlJSF9zE.5uKWeVH-7G8FIgkaLIhvzCROkWr4D3pMU0-tqk58EAQcLftyD2KBMIdYetjTYQEyQwWLy7Lfkm8cs3aogaThAw"
-        //var cert = "COYBEAUYASDyASiG2piD9zE.te46qua5ha2r-Caz03Vx2JXH5OLSRRV2GqdYcn9UslwibsxBSP98GhUKSGEI0Z84FRMkp16ZK8eS-y72QVE2AQ"
+
+//        var lines = "https://csapi.hfxg.xyz,https://xxxapi.qixin14.xyz"  // 服务器线路地址
+//        var cert = "COYBEAUYASDyASiG2piD9zE.te46qua5ha2r-Caz03Vx2JXH5OLSRRV2GqdYcn9UslwibsxBSP98GhUKSGEI0Z84FRMkp16ZK8eS-y72QVE2AQ"
+//        var baseUrlImage = "https://imagesacc.hfxg.xyz"  // 图片服务器地址
+
         var merchantId = 367  // 商户ID
         var userId = 666677   // 用户ID
-        //var baseUrlImage = "https://imagesacc.hfxg.xyz"  // 图片服务器地址
 
 
         //九月
